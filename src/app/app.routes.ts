3573e94ebe909da761fb25ba401cf110ae9ca9f5
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-06 13:16:43 +0545

    add a routes for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { routes } from './shared/routes/routes';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [...routes];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/shared/routes/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from '../../modules/about/about.component';
+import { ResumeComponent } from '../../modules/resume/resume.component';
+import { PortfolioComponent } from '../../modules/portfolio/portfolio.component';
+import { BlogComponent } from '../../modules/blog/blog.component';
+import { ContactComponent } from '../../modules/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '/about', component: AboutComponent },
+  {
+    path: '/resume',
+    component: ResumeComponent,
+  },
+  {
+    path: '/portfolio',
+    component: PortfolioComponent,
+  },
+  { path: '/blog', component: BlogComponent },
+  { path: '/contact', component: ContactComponent },
+];
