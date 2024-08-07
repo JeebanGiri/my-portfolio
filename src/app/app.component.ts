712abda02984ddab1bcc2baa5c6ae52c357908ec
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-07 15:00:25 +0545

    add profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AuthComponent } from './auth/auth.component';
 import { NavbarComponent } from './standalone-components/navbar/navbar.component';
 import { AboutComponent } from './modules/about/about.component';
 import { BlogComponent } from './modules/blog/blog.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthComponent, NavbarComponent],
+  imports: [RouterOutlet, AuthComponent, NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
