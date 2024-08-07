441982238c50e89ca7ec8b9a13a8dd20f29405da
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-07 09:13:45 +0545

    add a layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { NavbarComponent } from './standalone-components/navbar/navbar.component';
+import { AboutComponent } from './modules/about/about.component';
+import { BlogComponent } from './modules/blog/blog.component';
 
 @Component({
   selector: 'app-root',
