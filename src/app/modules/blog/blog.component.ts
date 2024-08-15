07ebe361561def06bdd3f1e0c68bde7f95776e60
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-15 19:57:37 +0545

    add blog content

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-
+  blogs = [
+    {
+      blog_category: 'Design',
+      date: 'July 23, 2024',
+      title: ' Design conferences in 2022',
+      description:
+        'Veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+      image: '../../../assets/images/blogs.jpeg',
+    },
+    {
+      blog_category: 'Design',
+      date: 'July 28, 2024',
+      title: 'Best Font for Design',
+      description:
+        'Veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+      image: '../../../assets/images/blogs.jpeg',
+    },
+  ];
 }
