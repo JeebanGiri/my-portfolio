ac3bd1175fba8210a05eab829c7b4dcb42aded6a
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-16 22:10:23 +0545

    add a angular googlr map" "

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { paperPlane } from 'ionicons/icons';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [IonIcon],
+  imports: [IonIcon, GoogleMap, MapInfoWindow, MapMarker],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
@@ -14,4 +16,9 @@ export class ContactComponent {
   constructor() {
     addIcons({ 'paper-plane': paperPlane });
   }
+
+  apiKey = environment.googleMapsApiKey;
+
+  center: google.maps.LatLngLiteral = { lat: 37.7749, lng: -122.4194 };
+  zoom = 12;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: 'AIzaSyC6UEyIGHKnjeKsWKNoOl3hsINZoJ7dalM', // Replace with your actual API key
+};
