8cab700366b0e65d5286000fcb1f4e41ea878c33
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-22 14:33:51 +0545

    add responsive for root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 import { NavbarComponent } from './standalone-components/navbar/navbar.component';
 import { AboutComponent } from './modules/about/about.component';
 import { BlogComponent } from './modules/blog/blog.component';
@@ -9,7 +8,7 @@ import { ProfileComponent } from './modules/profile/profile.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthComponent, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
