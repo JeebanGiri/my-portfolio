cb4366ba3212514913cf9cee1dcb63fca7ab03b3
Jeeban Giri <jeebangiri942@gmail.com>
2024-08-24 17:58:33 +0545

    refactor code

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronDown } from 'ionicons/icons';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [IonIcon],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  constructor() {
+    addIcons({ 'chevron-down': chevronDown });
+  }
   profilepath: string = '../../../assets/images/my-avatar.png';
   profilealt: string = 'Profile';
 }
