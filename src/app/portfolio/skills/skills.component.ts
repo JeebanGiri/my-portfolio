bc9218a1671bb4bfe32d7173956f149659abde97
Jeeban Giri <jeebangiri942@gmail.com>
2025-06-16 22:46:35 +0545

    add skill

--- src/app/portfolio/skills/skills.component.ts
+++ src/app/portfolio/skills/skills.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.css',
 })
 export class SkillsComponent {
+  clrList = ['red', 'yellow', 'green', 'blue'];
 
+  skills = [
+    { name: 'React JS', percent: 80, color: 'red' },
+    { name: 'Angular', percent: 75, color: 'yellow' },
+    { name: 'TypeScript', percent: 85, color: 'green' },
+    { name: 'CSS', percent: 90, color: 'blue' },
+  ];
 }
