15d0169640259f752c734376ce2643f8a4e5b936
Jeeban Giri <jeebangiri942@gmail.com>
2025-06-19 21:41:04 +0545

    fixed button issue

--- src/app/portfolio/education-experience-timeline/education-experience-timeline.component.ts
+++ src/app/portfolio/education-experience-timeline/education-experience-timeline.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { documentOutline } from 'ionicons/icons';
+import { BrowserService } from '../../shared/browser.service';
+import { IonIcon } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-education-experience-timeline',
   standalone: true,
-  imports: [],
+  imports: [IonIcon, CommonModule],
   templateUrl: './education-experience-timeline.component.html',
   styleUrl: './education-experience-timeline.component.css',
 })
 export class EducationExperienceTimelineComponent {
+  constructor(public browserService: BrowserService, private router: Router) {
+    addIcons({ 'document-outline': documentOutline });
+  }
   bookpath: string = '../../../assets/images/book.png';
   bookalt: string = 'Book';
 
   experiencePath: string = '../../../assets/images/experience.png';
   experiencealt: string = 'Experience';
+
+  goToResume() {
+    this.router.navigate(['/resume']);
+  }
+
+  downloadResume(){
+    
+  }
 }
--- src/app/portfolio/profile/profile.component.ts
+++ src/app/portfolio/profile/profile.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { IonIcon } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
+// import { IonIcon } from '@ionic/angular/standalone';
+// import { addIcons } from 'ionicons';
 import { chevronDown } from 'ionicons/icons';
 import { BrowserService } from '../../shared/browser.service';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [IonIcon],
+  imports: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
   constructor() {
-    addIcons({ 'chevron-down': chevronDown });
+    // addIcons({ 'chevron-down': chevronDown });
   }
   // profilepath: string = '../../../assets/images/my-avatar.png';
   profilepath: string = '../../../assets/images/profile.jpeg';
