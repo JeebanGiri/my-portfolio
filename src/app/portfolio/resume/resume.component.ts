0f2f9e9588637411f6671cdd32d78f24a66e1fb8
Jeeban Giri <jeebangiri942@gmail.com>
2025-06-20 16:05:32 +0545

    add resume

--- src/app/portfolio/resume/resume.component.ts
+++ src/app/portfolio/resume/resume.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
 })
 export class ResumeComponent {
   bookpath: string = '../../../assets/images/book.png';
   bookalt: string = 'Book';
+
+  timelineData = [
+    {
+      title: 'Backend Developer at Virtuosway',
+      date: 'Apr 2021 – Present',
+      description: 'Developing and maintaining backend services and APIs.',
+    },
+    {
+      title: 'Graduated from Tribhuvan University',
+      date: '2020',
+      description: 'Completed BSc in Computer Science.',
+    },
+    // Add more items...
+  ];
 }
