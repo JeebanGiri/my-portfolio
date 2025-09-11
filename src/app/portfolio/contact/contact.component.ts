acc4ee12e8cba8b289553b1155bd83eb65941a05
Jeeban Giri <jeebangiri942@gmail.com>
2025-09-11 22:00:49 +0545

    completed contact form

--- src/app/portfolio/contact/contact.component.ts
+++ src/app/portfolio/contact/contact.component.ts
@@ -6,11 +6,23 @@ import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { environment } from '../../../environments/environment';
 import { BrowserService } from '../../shared/browser.service';
 import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
+import { FormsModule } from '@angular/forms';
+import { service_ID } from '../../../environments/environment';
+import { template_ID } from '../../../environments/environment';
+import { public_KEY } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [IonIcon, GoogleMap, MapInfoWindow, MapMarker, CommonModule],
+  imports: [
+    IonIcon,
+    // GoogleMap,
+    // MapInfoWindow,
+    // MapMarker,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
@@ -23,4 +35,30 @@ export class ContactComponent {
 
   center: google.maps.LatLngLiteral = { lat: 26.6674487, lng: 87.3880129 };
   zoom = 14;
+
+  form = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  sendEmail() {
+    const formParams = {
+      name: this.form.name,
+      email: this.form.email,
+      message: this.form.message,
+    };
+
+    emailjs.send(service_ID, template_ID, formParams, public_KEY).then(
+      (response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Message Sent Successfully');
+        this.form = { name: '', email: '', message: '' };
+      },
+      (error) => {
+        console.log('FAILED...', error);
+        alert('Something went wrong. Please try again.');
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,7 @@ export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyC6UEyIGHKnjeKsWKNoOl3hsINZoJ7dalM', // Replace with your actual API key
 };
+
+export const service_ID = 'service_6gohn7h';
+export const template_ID = 'template_baw591h';
+export const public_KEY = 'wol484FBNd9Va0G0v';
