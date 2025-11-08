29de870178e6e4f246e82d6b33e4d2c9b7c7631a
Jeeban Giri <jeebangiri942@gmail.com>
2025-11-08 20:26:24 +0545

    make responsive for mobile

--- src/app/portfolio/profile/profile.component.ts
+++ src/app/portfolio/profile/profile.component.ts
@@ -1,28 +1,49 @@
 import { Component } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { documentOutline, eyeOutline } from 'ionicons/icons';
+import { chevronDownOutline, documentOutline, eyeOutline } from 'ionicons/icons';
 import { BrowserService } from '../../shared/browser.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonIcon],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
-  constructor(public browserService: BrowserService, private router: Router) {
-    addIcons({
-      'document-outline': documentOutline,
-      'eye-outline': eyeOutline,
-    });
-  }
+export class ProfileComponent implements OnInit {
+  isMobile = false;
+  isExpanded: boolean = false;
+
   profilepath: string = '../../../assets/images/profile.jpeg';
   profilealt: string = 'Profile';
 
-  isExpanded: boolean = false;
+  constructor(
+    public browserService: BrowserService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    addIcons({
+      'document-outline': documentOutline,
+      'eye-outline': eyeOutline,
+      'chevron-down-outline': chevronDownOutline
+    });
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateScreenSize();
+      window.addEventListener('resize', () => this.updateScreenSize());
+    }
+  }
+
+  updateScreenSize() {
+    this.isMobile = window.innerWidth <= 767;
+  }
 
   toggleInfo() {
     this.isExpanded = !this.isExpanded;
